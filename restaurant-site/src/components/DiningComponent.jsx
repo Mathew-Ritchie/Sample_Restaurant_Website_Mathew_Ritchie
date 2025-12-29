e1ed8950b0f5d9b56d6977419e34cd70b36fb76e
Mathew Ritchie <ritchmat89@gmail.com>
2025-12-29 08:58:37 +0000

    updated DiningComponent styling

--- restaurant-site/src/components/DiningComponent.jsx
+++ restaurant-site/src/components/DiningComponent.jsx
@@ -3,13 +3,13 @@ import React from "react";
 export default function DiningComponent({id}) {
   return (
     <div
-      className="w-full min-h-screen px-20 py-25 flex justify-center items-center bg-no-repeat bg-cover bg-center bg-fixed"
+      className="w-full h-screen px-50 py-25 flex justify-center items-center bg-no-repeat bg-cover bg-center bg-fixed"
       style={{
         backgroundImage: `url('images/Interior-dining.png')`,
       }}
       id={id}
     >
-      <div className="flex flex-col justify-center items-center relative z-10 md:w-full w-full bg-gray-800/40 text-white  p-8 overflow-y-auto min-h-screen">
+      <div className="flex flex-col justify-center items-center relative z-10 h-full md:w-full w-full bg-gray-800/40 text-white  p-8">
         <h1 className="text-4xl">An Incredible Dining Experience</h1>
         <h2 className="text-2xl"></h2>
       </div>
