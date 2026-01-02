49e1d79081cbf61fc0530065a9830abf856fa4c5
Mathew Ritchie <ritchmat89@gmail.com>
2026-01-02 13:09:13 +0000

    added responsive styling to Footer.

--- restaurant-site/src/components/Footer.jsx
+++ restaurant-site/src/components/Footer.jsx
@@ -4,32 +4,32 @@ import { Linkedin, Facebook, Instagram } from "lucide-react";
 export default function Footer({ id }) {
   return (
     <div
-      className="w-full min-h-screen px-20 py-10 flex justify-center items-center gap-10 bg-gray-900 text-white"
+      className="w-full min-h-screen px-10 py-10 flex justify-center items-center gap-10 bg-gray-900 text-white"
       id={id}
     >
       <div className="relative z-10 md:w-full w-full bg-gray-900 text-white p-8 overflow-y-auto ">
         <h1 className="text-4xl md:text-5xl font-extrabold mb-8 rounded-lg text-center">
           Contact Us
         </h1>
-        <div className="flex flex-col justify-center items-center gap-20">
-          <div className="flex justify-center items-center gap-10">
+        <div className="w-full flex flex-col justify-center items-center gap-20">
+          <div className="w-full flex flex-col sm:flex-row justify-center items-center gap-5">
             <img className="w-70" src={"/images/pizza-oven-sketch.png"} width={1500} height={1000} />
 
 
             {/* Contact Information Div */}
-            <div className="flex flex-col justify-start items-start space-y-6 text-gray-300 leading-relaxed">
+            <div className=" flex flex-col justify-start items-start space-y-6 text-gray-300 leading-relaxed">
               {/* Phone */}
-              <div className="flex justify-center items-center gap-2">
+              <div className=" flex justify-start items-center gap-2">
                 <img src="/icons/contact.gif" className="w-10" />
                 <p className="contact-details">+27 (000) 1111 222</p>
                 </div>
               {/* Email */}
-              <div className="flex justify-center items-center gap-2"> 
+              <div className=" flex justify-start items-center gap-2"> 
                 <img src="/icons/email.gif" className="w-10" />
                 <p className="contact-details">bestpizza@pizza.co.za</p>
                 </div>
               {/* Address */}
-              <div className="flex justify-center items-center gap-2">
+              <div className=" flex justify-start items-center gap-2">
                 <img src="/icons/google-maps.gif" className="w-10" />
                 <p className="contact-details">n.o. 1 One Street, Cape Town</p>
                 </div>
